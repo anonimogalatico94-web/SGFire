4d225a6e0a4b45159abc767f099c581280f05d1c
SGFire <anonimogalatico94@gmail.com>
2026-09-25 17:42:42 -0300

    SGFire: simplify Android A03 startup screen for reliable launch test

--- A03Test.gd
+++ A03Test.gd
@@ -1,13 +1,20 @@
 extends Control
 
 func _ready() -> void:
-    var start := Button.new()
-    start.text = "INICIAR TESTE 3D COM BOTS"
-    start.position = Vector2(230, 360)
-    start.size = Vector2(500, 80)
-    start.add_theme_font_size_override("font_size", 24)
-    start.pressed.connect(_start_game)
-    add_child(start)
+    var label := Label.new()
+    label.text = "SGFIRE\nTESTE A03\n\nToque no botão para entrar"
+    label.set_anchors_and_offsets_preset(Control.PRESET_CENTER)
+    label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+    label.add_theme_font_size_override("font_size", 28)
+    add_child(label)
+
+    var button := Button.new()
+    button.text = "ENTRAR NO TESTE"
+    button.size = Vector2(520, 100)
+    button.position = Vector2(220, 360)
+    button.add_theme_font_size_override("font_size", 26)
+    button.pressed.connect(_start_game)
+    add_child(button)
 
 func _start_game() -> void:
     get_tree().change_scene_to_file("res://Prototype.tscn")
